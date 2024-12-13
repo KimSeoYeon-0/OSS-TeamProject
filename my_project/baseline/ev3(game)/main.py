fe261ce1af5c1d8f9cc57ee4808d1c8f1fe1dbbd
KimSeoyeon <qwerz198@naver.com>
2024-12-13 13:00:17 +0900

    알고리즘 최적화 진행

--- my_project/baseline/ev3(game)/main.py
+++ my_project/baseline/ev3(game)/main.py
@@ -13,7 +13,6 @@ import time
 ev3 = EV3Brick()
 gyro = GyroSensor(Port.S3)
 ser = UARTDevice(Port.S2, baudrate=115200)
-ultra = UltrasonicSensor(Port.S4)
 
 #==========[motors]==========
 grab_motor = Motor(Port.B)
@@ -45,6 +44,20 @@ def turn2(target_angle, power):
             robot.drive(0, power)  # 시계 방향 회전
         else:
             robot.drive(0, -power)  # 반시계 방향 회전
+            
+def turn3(target_angle, power):
+    while True:
+        angle = gyro.angle()
+        angle_diff = (angle - target_angle + 180) % 360 - 180
+
+        # 목표 각도에 도달했는지 확인
+        if abs(angle_diff) < 2:  # 차이가 2도 이내라면 정지
+            robot.stop()
+            break
+        if angle_diff > 0:
+            robot.drive(0, -power)  # 반시계 방향 회전
+        else:
+            robot.drive(0, power)  # 시계 방향 회전
 
 
 #==========[camera_chase]==========
@@ -98,34 +111,19 @@ def shoot(command):
         
 def search_for_ball():
     print("공을 탐색 중...")
-    robot.straight(40)
     robot.turn(20)
+    robot.straight(40)
     start_time = time.time()
-    while True:  
+    while time.time() - start_time < 10:  
         data = ser.read_all()
         filter_result = process_uart_data(data)
+        print(filter_result)
         if filter_result[0] != -1 and filter_result[1] != -1:
             pd_control(filter_result[0], kp=0.5, kd=0.3, power=100)
-           
+            print(filter_result)
             return filter_result
-        print("공을 찾지 못했습니다.")
-        return [-1, -1]
-
-def ultra_distance():
-    while True:
-        data = ser.read_all()
-        filter_result = process_uart_data(data)
-        distance = ultra.distance()
-        if distance <= 100:
-            robot.straight(-100)
-            robot.turn(-50)
-            time.sleep(0.25)
-        else:
-            robot.straight(80)
-            robot.turn(30)
-            time.sleep(0.25)
-            return filter_result
-
+    print("공을 찾지 못했습니다.")
+    return [-1, -1]
 
 #==========[setup]==========
 ev3.speaker.beep()
@@ -151,7 +149,7 @@ while True:
             robot.straight(100) #앞으로 이동
             grab('motion3') #공을 잡기
             time.sleep(0.5) #동작간 딜레이
-            turn2(0,100) #정면(상대방 진영)바라보기
+            turn3(0,100) #정면(상대방 진영)바라보기
             time.sleep(2) #동작간 딜레이
             grab('motion1') #슛을 위한 열기
             time.sleep(0.5) #동작간 딜레이
